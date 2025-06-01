3c03ec5d9fc50af51b8c82bb098eccc40d2ff49d
NoorHafowBare <noorhafowbare@gmail.com>
2025-06-01 13:48:20 +0300

    initial commit

--- 1. Beginners/04. Input/live coding/index.py
+++ 1. Beginners/04. Input/live coding/index.py
@@ -1,5 +1,7 @@
 user_input = input("Enter something: ") # returns "string"
 print("You entered:", user_input)
+user_name = input("what is your name: ")
+print("your name is", user_name)
 
 
 # --------------------------
@@ -8,3 +10,4 @@ user_number = int(input("How old are you: "))
 # Perform some operation with the input
 result = user_number * 2
 print("Twice the entered number:", result)
+
